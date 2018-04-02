ecba43e5ef0e471c3a12ac7715e6dc7cee80e2fb
ashab <asha.ab@gmail.com>
2018-04-01 21:24:01 -0400

    Center align the question
    TODO: Understand how this works -
    https://stackoverflow.com/questions/1582284/how-to-position-an-input-form-at-the-center-of-a-page

--- src/question.js
+++ src/question.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Question extends React.Component {
   render() {
     return (
-      <form>
-        <label>
-          What is the largest organ of the human body?
-          <input type="text" value="" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div className="QuestionContainer">
+        <form className="QuestionForm">
+          <label>
+            What is the largest organ of the human body?
+            <div className="Answer">
+              <input type="text" name="answer"/>
+            </div>
+          </label>
+          <div className="SubmitContainer">
+            <input type="submit" value="Submit" />
+          </div>
+        </form>
+      </div>
     );
   }
 }
